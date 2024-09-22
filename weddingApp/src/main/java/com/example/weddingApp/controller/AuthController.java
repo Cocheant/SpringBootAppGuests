62c69e6d8f90eb29c8c9bc41df235c226a8f5309
Antoine Cochet <entonia01@gmail.com>
2024-09-22 23:52:43 +0200

    jwt token validation

--- weddingApp/src/main/java/com/example/weddingApp/controller/AuthController.java
+++ weddingApp/src/main/java/com/example/weddingApp/controller/AuthController.java
@@ -7,15 +7,20 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.weddingApp.dto.LoginRequest;
 import com.example.weddingApp.security.JwtProvider;
 import com.example.weddingApp.service.UserService;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
@@ -31,6 +36,7 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
         try {
@@ -54,5 +60,28 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
         }
 
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+    @GetMapping("/token")
+    public ResponseEntity<?> getAdminPage(HttpServletRequest request) {
+        // Check cookies for JWT
+        String jwt = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("jwt")) {
+                    jwt = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (jwt == null || !jwtProvider.validateToken(jwt)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Unauthorized");
+        }
+
+        // Proceed if the JWT is valid
+        return ResponseEntity.ok("Valid Auth");
     }
 }
